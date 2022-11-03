46ab14f61032e012f5980e6f8bdbc4b4558574b1
Rivenrem <rivensenik@gmail.com>
2022-11-03 21:35:29 +0300

    feat: add debouncer in header's input

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,33 +2,31 @@ import styles from "./header.module.scss";
 import { useState, useContext } from "react";
 import apiFetcher from "../helpers/apiFetcher";
 import { NewsContext } from "../contexts/news.context";
+import debounce from "../helpers/debounce";
+// import Spinner from "./Spinner";
 
 export default function Header() {
-  const [inputValue, setInputValue] = useState(0);
-
+  const [isSearching, setIsSearching] = useState(false);
   const { setNews } = useContext(NewsContext);
+
+  async function inputHandler(event) {
+    if (!event.target.value) return;
+    setIsSearching(true);
+    setNews(await apiFetcher(event.target.value));
+    setIsSearching(false);
+  }
 
   return (
     <div className={styles.header}>
       <input
-        onChange={(event) => {
-          setInputValue(event.target.value);
-        }}
+        onChange={debounce(inputHandler, 500)}
         className={styles["header__input"]}
         type="textarea"
         name="input"
         autoComplete="off"
         placeholder="Search..."
       ></input>
-
-      <button
-        onClick={async () => {
-          setNews(await apiFetcher(inputValue));
-        }}
-        className={styles["header__submitbutton"]}
-      >
-        Go!
-      </button>
+      {isSearching && <div className={styles["header__spinner"]}></div>}
     </div>
   );
 }
