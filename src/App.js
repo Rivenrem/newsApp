1fa709c41dffdba6c9d369690e1d32ec8a5eab5d
Rivenrem <rivensenik@gmail.com>
2022-11-16 13:40:01 +0300

    refactor: move BrowserRouter inside App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./app.module.scss";
 import { NewsProvider } from "./contexts/news.context";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Index from "./pages/Index";
 import SingleArticle from "./pages/SingleArticle";
 export default function App() {
   return (
     <div>
-      <NewsProvider>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path={`/article/:id`} element={<SingleArticle />} />
-        </Routes>
-      </NewsProvider>
+      <BrowserRouter>
+        <NewsProvider>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path={`/article/:id`} element={<SingleArticle />} />
+          </Routes>
+        </NewsProvider>
+      </BrowserRouter>
     </div>
   );
 }
