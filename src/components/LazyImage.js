b83aecd2c15b6e30c9780d9ab74abee0e62b4128
Rivenrem <rivensenik@gmail.com>
2022-12-21 17:34:25 +0300

    refactor: refactoring for mr

--- src/components/LazyImage.js
+++ src/components/LazyImage.js
@@ -1,18 +1,37 @@
 import styles from "./lazyImage.module.scss";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import Spinner from "./Spinner";
 
 export default function LazyImage({ src, alt }) {
   const imageRef = useRef(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    const io = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const image = entry.target;
-          image.src = src;
-        }
-      });
-    });
+    const io = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const image = entry.target;
+            image.src = src;
+            observer.disconnect();
+          }
+        });
+      },
+      { rootMargin: "100px" }
+    );
     io.observe(imageRef.current);
   }, []);
-  return <img className={styles.image} alt={alt} ref={imageRef}></img>;
+
+  return (
+    <div className={styles["lazy-image"]}>
+      {loading ? <Spinner className={styles["lazy-image__spinner"]} /> : null}
+
+      <img
+        className={styles["lazy-image__image"]}
+        alt={alt}
+        ref={imageRef}
+        onLoad={() => setLoading(false)}
+      />
+    </div>
+  );
 }
